5ceff5f06f4f2193af3a567f35c9fa3a92ae677b
nledany <nledany@ebay.com>
2020-10-18 01:58:36 +0300

    Remove localhost

--- src/service/API.js
+++ src/service/API.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://54.157.243.13:8080/",
-  baseURL: "https://localhost:8080/",
+  baseURL: "https://54.157.243.13:8080/",
   responseType: "json",
   timeout: 120000
 });
